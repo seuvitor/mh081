495fd0f5c2ed9b0b800b76ecdb21d58dd8ea3e30
seuvitor <seuvitor@d702bc78-094b-0410-95f7-1f72f422bb33>
2008-08-02 17:21:13 +0000

    A string buffer is now used to avoid repeated concatenations of very large strings.

--- src/reports.py
+++ src/reports.py
@@ -58,18 +58,18 @@ def report_compiled_results(problem, compiled_results_list):
     report_title += ' ' + timestamp.strftime('%d/%m/%Y %Hh%M')
 
     # Insert latex header
-    text = ''
-    text += '\\documentclass{article}\n'\
+    buffer = []
+    buffer.append('\\documentclass{article}\n'\
             '\\usepackage{fullpage}\n'\
             '\\usepackage[brazil]{babel}\n'\
             '\\usepackage[latin1]{inputenc}\n'\
             '\\usepackage{graphicx}\n'\
-            '\\title{Experiment report\\\\\\small{\\textbf{'
-    text += report_title
-    text += '}}}\n'\
+            '\\title{Experiment report\\\\\\small{\\textbf{'\
+            + report_title \
+            +'}}}\n'\
             '\\date{}\n'\
             '\\begin{document}\n'\
-            '\\maketitle\n'
+            '\\maketitle\n')
     
     # Calculate how many tables will be needed
     DEFAUL_TABLE_LINE_LIMIT = 20
@@ -78,43 +78,40 @@ def report_compiled_results(problem, compiled_results_list):
     num_tables = num_instances / table_limit
     if num_instances % table_limit != 0: num_tables += 1
 
-    text += '\\section{Tables}\n'
+    buffer.append('\\section{Tables}\n')
     
     # For each table
     for i in range(num_tables):
         #compiled_results_group = compiled_results[i*table_limit:(i+1)*table_limit]
 
         # Write table header
-        table = ''
-        table += '\\begin{center}\n'\
-                 '\\begin{tabular}{| r r '
-        table += ('| r r r ' * num_algorithms)
-        table += '|}\n'\
+        buffer.append('\\begin{center}\n'\
+                 '\\begin{tabular}{| r r '\
+                 + ('| r r r ' * num_algorithms)\
+                 + '|}\n'\
                  '\\hline\n'\
-                 '\\multicolumn{2}{| c |}{} '
+                 '\\multicolumn{2}{| c |}{} ')
         
         for (algorithm_name, compiled_results, screen_output) in compiled_results_list:
-            table += ' & \\multicolumn{3}{| c |}{' + algorithm_name + '} '
+            buffer.append(' & \\multicolumn{3}{| c |}{' + algorithm_name + '} ')
         
-        table += '\\\\\n'
-                 
-        table += 'instance    &$z*$    '
-        table += ('&$z$ &gap    &time ' * num_algorithms)
-        table += '\\\\\n'\
-                 '\\hline\n'
+        buffer.append('\\\\\n'\
+                 + 'instance    &$z*$    '\
+                 + ('&$z$ &gap    &time ' * num_algorithms)\
+                 + '\\\\\n'\
+                 '\\hline\n')
 
         # Fill table contents
         begin = i * table_limit
         end = min(num_instances, ((i + 1) * table_limit)) - 1
         
-        table_entries = ''
         for j in range(begin, (end + 1)):
             (instance_name, opt_value, tmp_v, tmp_g, tmp_t) = first_compiled_results[j]
             instance_name = instance_name.replace('_','\_')
             if opt_value != None:
-                table_entries += '%s & %.0f ' % (instance_name, opt_value)
+                buffer.append('%s & %.0f ' % (instance_name, opt_value))
             else:
-                table_entries += '%s & ? ' % (instance_name)
+                buffer.append('%s & ? ' % (instance_name))
             
             # Fill columns with results from different algorithms
             for (algorithm_name, compiled_results, screen_output) in compiled_results_list:
@@ -122,59 +119,58 @@ def report_compiled_results(problem, compiled_results_list):
                 (tmp_i, tmp_o, best_value, percentual_gap, total_time) = compiled_results[j]
                 
                 if percentual_gap == 0.0:
-                    table_entries += '& \\textbf{%.0f} & %.2f & %.2f ' %\
-                        (best_value, percentual_gap, total_time)
+                    buffer.append('& \\textbf{%.0f} & %.2f & %.2f ' %\
+                        (best_value, percentual_gap, total_time))
                 elif percentual_gap != None:
-                    table_entries += '& %.0f & %.2f & %.2f ' %\
-                        (best_value, percentual_gap, total_time)
+                    buffer.append('& %.0f & %.2f & %.2f ' %\
+                        (best_value, percentual_gap, total_time))
                 else:
-                    table_entries += '& %.0f & ? & %.2f ' %\
-                        (best_value, total_time)
+                    buffer.append('& %.0f & ? & %.2f ' %\
+                        (best_value, total_time))
                         
-            table_entries += '\\\\\n'
+            buffer.append('\\\\\n')
             
         # Finish table
-        table += table_entries
-        table += '\\hline\n'\
+        buffer.append('\\hline\n'\
                  '\\end{tabular}\n'\
                  '\\end{center}\n'\
-                 '\\clearpage\n'
-        
-        text += table
+                 '\\clearpage\n')
     
     # Insert graphs for each instance and algorithm
-    text += '\\section{Figures}\n'
+    buffer.append('\\section{Figures}\n')
     for (algorithm_name, compiled_results, screen_output) in compiled_results_list:
-        text += '\\subsection{Figures for algorithm ' + algorithm_name + '}\n'
+        buffer.append('\\subsection{Figures for algorithm ' + algorithm_name + '}\n')
         even = False
         for (instance_name, opt_value, tmp_v, tmp_g, tmp_t) in first_compiled_results:
-            text += '\\begin{figure}[hb!]\n'\
+            buffer.append('\\begin{figure}[hb!]\n'\
                     '\\centering\n'\
                     '\\includegraphics[scale=0.6]{' + figure_name(instance_name, algorithm_name) + '}\n'\
                     '\\caption{' + problem.get_problem_name() + ' ' + algorithm_name + ' ' + instance_name + '}\n'\
-                    '\\end{figure}\n'
+                    '\\end{figure}\n')
         
-            if even: text += '\\clearpage\n'
+            if even:
+                buffer.append('\\clearpage\n')
             even = not even
         
-        if even: text += '\\clearpage\n'
+        if even:
+            buffer.append('\\clearpage\n')
     
 
     # Insert screen output for each algorithm at the end of the document
-    text += '\\section{Execution logs}\n'
+    buffer.append('\\section{Execution logs}\n')
     for (algorithm_name, compiled_results, screen_output) in compiled_results_list:
-        text += '\\subsection{Detailed output of algorithm ' + algorithm_name + '}\n'
-        text += '\\scriptsize\n'
-        text += '\\begin{verbatim}\n'
-        text += screen_output
-        text += '\\end{verbatim}\n'\
-                '\\clearpage\n'
+        buffer.append('\\subsection{Detailed output of algorithm ' + algorithm_name + '}\n'\
+                '\\scriptsize\n'\
+                '\\begin{verbatim}\n'\
+                + screen_output\
+                + '\\end{verbatim}\n'\
+                '\\clearpage\n')
         
-    text += '\\end{document}'
+    buffer.append('\\end{document}')
 
     # Save report file
     output_file = open(REPORTS_DIR + report_file_name, 'w')
-    output_file.write(text)
+    output_file.write(''.join(buffer))
     output_file.close()
     
     import os
